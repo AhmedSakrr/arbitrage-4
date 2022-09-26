77b2d3285e1a7694dde6678a7fd98f50b245a1e9
GVatest <vasiliyganja@Vasiliys-MacBook-Air.local>
2022-09-26 18:01:44 +0300

    return is_staff field to user model

--- accounts/models.py
+++ accounts/models.py
@@ -19,6 +19,7 @@ class UserAccountManager(BaseUserManager):
         user = self.create_user(username, password)
 
         user.is_superuser = True
+        user.is_staff = True
         user.save()
 
         return user
@@ -26,6 +27,7 @@ class UserAccountManager(BaseUserManager):
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserAccountManager()
 
