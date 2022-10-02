1a90099e21181e7acf68dea12d59ee50d03e7b23
Gvatest <ganjatriller@gmail.com>
2022-10-02 20:26:08 +0300

    force all time display scrollbar

--- src/components/Flows.jsx
+++ src/components/Flows.jsx
@@ -40,7 +40,7 @@ const Flows = () => {
                             {traffic.map((item, i) => (
                                 <tr className='flows__items__item' key={i}>
                                     <th><img src={trafficTypesToInfo[item.type][0]} alt="Traffic type" /></th>
-                                    <th className='flows__items__item__id'><span className='block_bg'>ID {item.user.length > 9 ? item.user.slice(0, 7) + "..." : item.user}</span></th>
+                                    <th className='flows__items__item__id'><span className='block_bg'><nobr>ID {item.user.length > 9 ? item.user.slice(0, 7) + "..." : item.user}</nobr></span></th>
                                     <th>{item.type === "BT" || item.type === "PT" ? "+" : null}{item.amount}$</th>
                                     <th>{trafficTypesToInfo[item.type][1]}</th>
                                     <th className='flows__items__item__date'>{item.date.slice(5, item.date.length).split("T")[0].split("-").join(".")}</th>
